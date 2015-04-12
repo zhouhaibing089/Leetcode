a521f10e13d73570ea272d5f42a420d60559f1ab
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-04-12 15:37:45 +0800

    js solution of two sum

---
+++ 1/js.js
@@ -0,0 +1,36 @@
+var twoSum = function(numbers, target) {
+    var required = [];
+    var hashed = {};
+
+    numbers.forEach(function(value, index) {
+        required.push(target - value);
+        if (hashed[value + ""] === undefined) {
+            hashed[value + ""] = [index];
+        } else {
+            hashed[value + ""].push(index);
+        }
+    });
+
+    required = required.map(function(value) {
+        return hashed[value + ""];
+    });
+
+    for (var i = 0; i < required.length; ++i) {
+        var value = required[i];
+
+        if (value === undefined) {
+            continue;
+        }
+        if (value.length === 1) {
+            if (value[0] == i) {
+                continue ;
+            }
+            return [i + 1, value[0] + 1];
+        }
+        if (i === value[0]) {
+            return [i + 1, value[1] + 1];
+        }
+    }
+};
+
+console.log(twoSum([3, 2, 4], 6));
