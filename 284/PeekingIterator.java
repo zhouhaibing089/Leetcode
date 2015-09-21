d962283fd738dace40e5d241deb4099444b7d6f8
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-09-21 14:41:13 +0800

    peekingiterator accepted

---
+++ 284/PeekingIterator.java
@@ -0,0 +1,39 @@
+class PeekingIterator implements Iterator<Integer> {
+
+	Iterator<Integer> mIterator;
+
+    Integer mPeeked = null;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        mIterator = iterator;
+    }
+
+    public Integer peek() {
+        if (mPeeked != null) {
+            return mPeeked;
+        }
+        if (hasNext()) {
+            mPeeked = next();
+            return mPeeked;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return mPeeked != null || mIterator.hasNext();
+    }
+
+    @Override
+    public Integer next() {
+        Integer value;
+        if (mPeeked != null) {
+            value = mPeeked;
+            mPeeked = null;
+        } else {
+            value = mIterator.next();
+        }
+        return value;
+    }
+}
