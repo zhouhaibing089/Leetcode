37cceab58b909b7b333ea2f28776b5c4593ed588
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-10-02 11:45:53 +0800

    delete node accepted

---
+++ 237/deletenode.c
@@ -0,0 +1,12 @@
+
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
+
+void deleteNode(struct ListNode* node) {
+  struct ListNode* next = node->next;
+  node->val = next->val;
+  node->next = next->next;
+  free(next);
+}
