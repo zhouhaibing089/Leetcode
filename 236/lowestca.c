a9ca239ad40643c565c52857a1a46b6deed1bb5e
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-10-02 12:58:37 +0800

    lowest common accestor accepted

---
+++ 236/lowestca.c
@@ -0,0 +1,22 @@
+
+struct TreeNode {
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+  if (root == NULL || p == root || q == root) {
+    return root;
+  }
+  // search left
+  struct TreeNode* left = lowestCommonAncestor(root->left, p, q);
+  struct TreeNode* right = lowestCommonAncestor(root->right, p, q);
+  if (left == NULL) {
+    return right;
+  }
+  if (right == NULL) {
+    return left;
+  }
+  return root;
+}
