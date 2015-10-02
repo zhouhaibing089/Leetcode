6a89656f805eb6a1e7d0090c1163dccb35ddb4f1
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-10-02 14:31:23 +0800

    little update

--- 268/misnum2.c
+++ 268/misnum2.c
@@ -1,8 +1,7 @@
 int missingNumber(int* nums, int numsSize) {
   int xor = 0;
   for (int i = 0; i < numsSize; ++i) {
-    xor ^= i;
-    xor ^= nums[i];
+    xor ^= (i ^ nums[i]);
   }
   return xor ^ numsSize;
 }
