af45505da3da1134f4ca8e4fb6124bb684b08339
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-10-02 22:25:13 +0800

    power of two accepted

---
+++ 231/poweroftwo.c
@@ -0,0 +1,13 @@
+
+bool isPowerOfTwo(int n) {
+  if (n <= 0) {
+    return false;
+  }
+  if (n == 1) {
+    return true;
+  }
+  if (n % 2 == 0) {
+    return isPowerOfTwo(n / 2);
+  }
+  return false;
+}
