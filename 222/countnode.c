57f3f81a63e44206067249b87cd324bef9cb180d
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-10-04 16:14:32 +0800

    count node first version accepted

---
+++ 222/countnode.c
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int left_level(struct TreeNode* root) {
+  if (root == NULL) {
+    return 0;
+  }
+  return 1 + left_level(root->left);
+}
+
+int right_level(struct TreeNode* root) {
+  if (root == NULL) {
+    return 0;
+  }
+  return 1 + right_level(root->right);
+}
+
+int countNodes2(struct TreeNode* root, int ll, int rl) {
+  if (root == NULL) {
+    return 0;
+  }
+  if (ll == -1) {
+    ll = left_level(root);
+  }
+  if (rl == -1) {
+    rl = right_level(root);
+  }
+  /* full */
+  if (ll == rl) {
+    int val = pow(2, ll ) - 1;
+    return val;
+  }
+  int rtl = right_level(root->left);
+  if (ll == (rtl + 1)) {
+    /* recursion to right */
+    int val = countNodes2(root->right, -1, rl - 1);
+    return val + pow(2, ll - 1);
+  } else {
+    /* recursion to left */
+    int val = countNodes2(root->left, ll - 1, rtl);
+    return val + pow(2, rl - 1);
+  }
+}
+
+int countNodes(struct TreeNode* root) {
+  return countNodes2(root, -1, -1);
+}
